045baf2522e011ee1a20e233da9c54eac15dd984
Artem <a.kotov@nord-soft.com>
2020-04-06 14:33:15 +0300

    create Providers Component

--- src/index.js
+++ src/index.js
@@ -3,32 +3,9 @@ import "./ReactotronConfig";
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "mobx-react";
-import { ThemeProvider } from "styled-components";
-
-// Store
-import { store } from "./store/rootStore";
-// Instruments
-import "./index.css";
 // Components
-import App from "./components/App";
+import AppProvider from "./components/AppProvider";
 
-const theme = {
-  direction: "rtl",
-};
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider {...store}>
-      <Router>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<AppProvider />, document.getElementById("root"));
 
 serviceWorker.unregister();
